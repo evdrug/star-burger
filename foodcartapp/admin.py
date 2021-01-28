113f1556361e89c4bec02bf64bb26c0de17bc2f1
druzyakin.e <druzyakin.e@gmail.com>
2021-01-28 10:05:30 +0300

    redirect only to ALLOWED_HOSTS

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
+from star_burger.settings import ALLOWED_HOSTS
 from .models import Order, OrderElements
 from .models import Product
 from .models import ProductCategory
@@ -132,7 +134,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     def response_post_save_change(self, request, obj):
         res = super().response_post_save_change(request, obj)
-        if "next" in request.GET:
+        if url_has_allowed_host_and_scheme(request.GET['next'], ALLOWED_HOSTS):
             return HttpResponseRedirect(request.GET['next'])
         else:
             return res
